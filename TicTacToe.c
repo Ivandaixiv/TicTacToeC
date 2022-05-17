2a9984f3015d44299a1b4d6d65622c11727f63c9
Ivan <ivandaixivwork@gmail.com>
2022-05-17 13:14:12 -0700

    Add user input for rows and columns

--- TicTacToe.c
+++ TicTacToe.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+typedef enum
+{
+    true,
+    false
+} bool;
+
 int main()
 {
-    printf("Hello World");
+    int userRows, userCol;
+    printf("How many rows do you want to play?: ");
+    scanf("%d", &userRows);
+
+    printf("How many columns do you want to play?: ");
+    scanf("%d", &userCol);
+
+    printf("userRows: %d , userCol: %d\n", userRows, userCol);
+    // bool gridRow[3];
 }
