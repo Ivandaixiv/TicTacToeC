ffab352ab5d045d3f69bd7c7ce0eed42382e4159
Ivan <ivandaixivwork@gmail.com>
2022-05-18 10:33:41 -0700

    Add display of grid by user selected size

--- TicTacToe.c
+++ TicTacToe.c
@@ -15,6 +15,22 @@ int main()
     printf("How many columns do you want to play?: ");
     scanf("%d", &userCol);
 
+    printf(" ");
+    for (int i = 1; i <= userCol; i++)
+    {
+        printf(" %d", i);
+    }
+    printf("\n");
+
+    for (int i = 0; i < userRows; i++)
+    {
+        printf("%d ", i + 1);
+        for (int j = 0; j < userCol; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
     printf("userRows: %d , userCol: %d\n", userRows, userCol);
     // bool gridRow[3];
 }
