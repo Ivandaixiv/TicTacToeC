6d2fc59888ca42670235c88bd70125e5d1313b40
Ivan <ivandaixivwork@gmail.com>
2022-05-18 11:01:27 -0700

    Add grid display in alphabet

--- TicTacToe.c
+++ TicTacToe.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * TODO
+ *
+ * Limit user size to size of alphabet
+ */
 typedef enum
 {
     true,
@@ -16,21 +20,42 @@ int main()
     scanf("%d", &userCol);
 
     printf(" ");
-    for (int i = 1; i <= userCol; i++)
+    for (int i = 0; i < userCol; i++)
     {
-        printf(" %d", i);
+        printf(" %d", i + 1);
     }
     printf("\n");
 
+    int ticTacGrid[userRows][userCol];
     for (int i = 0; i < userRows; i++)
     {
-        printf("%d ", i + 1);
         for (int j = 0; j < userCol; j++)
         {
-            printf("* ");
+            ticTacGrid[i][j] = -1;
+        }
+    }
+
+    for (int i = 0; i < userRows; i++)
+    {
+        char colConvertChar = (char)(i + 65);
+        printf("%c ", colConvertChar);
+        // printf("%d ", i + 1);
+        for (int j = 0; j < userCol; j++)
+        {
+            if (ticTacGrid[i][j] == 0)
+            {
+                printf("O ");
+            }
+            else if (ticTacGrid[i][j] == 1)
+            {
+                printf("X ");
+            }
+            else
+            {
+                printf("_ ");
+            }
         }
         printf("\n");
     }
     printf("userRows: %d , userCol: %d\n", userRows, userCol);
-    // bool gridRow[3];
 }
